7f97eb9c61b2bd287c1d562a34adc9f11af1eec8
Shubham Maurya <risjiomaujio7654@gmail.com>
2022-09-13 09:05:44 +0530

    adding resources

---
+++ assets/games/Jo__Enemy__Killer/Resources/Sprite Command/assets/js/spriteanimation.js
@@ -0,0 +1,30 @@
+spriteAnimations = [
+    'idle' = {
+       loc: [
+        {x:0, y:0},
+        {x:575, y:0},
+        {x:1150, y:0},
+        {x:1725, y:0},
+        {x:2300, y:0},
+        {x:2875, y:0},
+        {x:3450, y:0}
+
+       ]
+
+    },
+    'jump' = {
+        loc: [
+            
+        ]
+
+    },
+    'run' = {
+       loc: [
+        
+       ]
+
+    }
+]
+
+console.log(spriteAnimations['idle'].loc[2].x)
+console.log(spriteAnimations['idle'].loc.length)
