486decb13e274f9330af12794a9537881fb209b6
Shubham Maurya <risjiomaujio7654@gmail.com>
2022-09-13 18:17:11 +0530

    collision Detection

--- assets/games/Jo__Enemy__Killer/Resources/NPC Movement Animations/assets/js/main.js
+++ assets/games/Jo__Enemy__Killer/Resources/NPC Movement Animations/assets/js/main.js
@@ -3,7 +3,7 @@ const canavs = document.getElementById('canvas1')
 const ctx = canavs.getContext('2d')
 CANVAS_WIDTH = canavs.width = 500
 CANVAS_HEIGHT = canavs.height = 1000
-const numberofEnemies = 50
+const numberofEnemies = 30
 const enemiesArray = []
 let gameFrame = 0
 
@@ -12,28 +12,33 @@ let gameFrame = 0
 class Enemy {
     constructor() {
         this.image = new Image()
-        this.image.src = './assets/img/enemies/enemy3.png'
+        this.image.src = './assets/img/enemies/enemy4.png'
      
         this.speed = Math.random() * 4 + 1
-        this.spriteWidth = 218
-        this.spriteHeight = 177
+        this.spriteWidth = 213
+        this.spriteHeight = 213
         this.width = this.spriteWidth / 3
         this.height = this.spriteHeight / 2.1
         this.x = Math.random() * (canavs.width - this.width)
         this.y = Math.random() * (canavs.height - this.height)
-
+        this.newX = Math.random() * (canavs.width - this.width)
+        this.newY = Math.random() * (canavs.height - this.height)
         this.frame = 0
         this.flapSpeed = Math.floor(Math.random() * 3 + 1)
-        this.angle = Math.random() * 500
-        this.angleSpeed = Math.random() * .5 + 0.5
-        // this.curve = Math.random() * 200 + 50
-
+        this.interval = Math.floor(Math.random() * 200 + 50)
+     
     }
     update(){
-        this.x = canavs.width/2 * Math.cos(this.angle * Math.PI/200) + (canavs.width/2 - this.width/2)
+        if(gameFrame % this.interval === 0){
+            this.newX = Math.random() * (canavs.width - this.width)
+            this.newY = Math.random() * (canavs.height - this.height)
+        }
+        let dx = this.x - this.newX
+
         
-        this.y = canavs.height/2 * Math.sin(this.angle * Math.PI/300) + (canavs.height/2 - this.height/2)
-        this.angle += this.angleSpeed
+        let dy = this.y - this.newY
+        this.x -= dx/70
+        this.y -= dy/70
         if(this.x + this.width < 0) this.x = canavs.width
         if(gameFrame % this.flapSpeed === 0){
             this.frame > 4 ? this.frame = 0 : this.frame++
