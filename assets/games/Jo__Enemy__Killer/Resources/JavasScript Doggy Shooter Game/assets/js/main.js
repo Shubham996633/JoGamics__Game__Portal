5af7684a8a24e28ce0bc746611e158626854beb5
Shubham Maurya <risjiomaujio7654@gmail.com>
2022-09-16 22:23:52 +0530

    adding game

--- assets/games/Jo__Enemy__Killer/Resources/JavasScript Doggy Shooter Game/assets/js/main.js
+++ assets/games/Jo__Enemy__Killer/Resources/JavasScript Doggy Shooter Game/assets/js/main.js
@@ -1,5 +1,7 @@
 import {Player} from './player.js'
 import {InputHandler} from './input.js'
+import {Background} from './background.js'
+import { FlyingEnemy, ClimbingEnemy, GroundEnemy } from './enemies.js'
 
 window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1')
@@ -11,29 +13,66 @@ window.addEventListener('load', function(){
         constructor(width, height){
             this.width = width
             this.height = height
+            this.groundMargin = 80
+            this.speed = 0
+            this.maxSpeed = 4
+            this.background = new Background(this)
             this.player = new Player(this)
             this.input = new InputHandler()
+            this.enemies = []
+            this.enemyTimer = 0
+            this.enemyInterval = 1000
         }
-        update(){
-            this.player.update()
+        update(deltaTime){
+            this.background.update()
+            this.player.update(this.input.keys, deltaTime)
+            if(this.enemyTimer > this.enemyInterval){
+                this.addEnemy()
+                this.enemyTimer = 0
+
+            }else {
+                this.enemyTimer += deltaTime
+            }
+            this.enemies.forEach(enemy => {
+                enemy.update(deltaTime)
+                if(enemy.markedForDeletion) this.enemies.splice(this.enemies.indexOf(enemy), 1)
+            })
+
+
+
 
         }
 
         draw(context){
+            this.background.draw(context)
+            this.enemies.forEach(enemy => {
+                enemy.draw(context)
+            })
+
             this.player.draw(context)
+
+        }
+
+        addEnemy(){
+            if(this.speed > 0 && Math.random() < 0.5) this.enemies.push(new GroundEnemy(this))
+            else if (this.speed > 0) this.enemies.push(new ClimbingEnemy(this))
+            this.enemies.push(new FlyingEnemy(this))
+            console.log(this.enemies)
 
         }
     }
 
     const game = new Game(canvas.width, canvas.height)
-
-    function animate(){
+    let lastTime = 0
+    function animate(timeStamp){
+        const deltaTime = timeStamp - lastTime
+        lastTime = timeStamp
         ctx.clearRect(0, 0, canvas.width, canvas.height)
-        game.update()
+        game.update(deltaTime)
         game.draw(ctx)
         requestAnimationFrame(animate)
     }
 
-    animate()
+    animate(0)
 
 })
